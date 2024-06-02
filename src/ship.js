d0fd88d7d78bc8e828b249d07aa070c5734a0a54
chochimus <127354521+chochimus@users.noreply.github.com>
2024-06-01 22:06:06 -0400

    Create ship class

---
+++ src/ship.js
@@ -0,0 +1,14 @@
+export class Ship {
+  constructor(length = 1) {
+    this.length = length;
+    this.timesHit = 0;
+  }
+  hit() {
+    if (!this.isSunk()) {
+      this.timesHit += 1;
+    }
+  }
+  isSunk() {
+    return this.timesHit >= this.length;
+  }
+}
--- src/test.js
+++ src/test.js
@@ -1,3 +1,35 @@
-test("example", () => {
-  expect(2 + 2).toBe(4);
+import { Ship } from "./ship";
+
+describe("Ship class", () => {
+  it("should create a ship with a length property", () => {
+    const ship = new Ship(5);
+    expect(ship.length).toBe(5);
+  });
+
+  it("should have a timesHit property initialized to 0", () => {
+    const ship = new Ship(5);
+    expect(ship.timesHit).toBe(0);
+  });
+
+  it("should increment timesHit when hit method is called", () => {
+    const ship = new Ship(3);
+    ship.hit();
+    expect(ship.timesHit).toBe(1);
+    ship.hit();
+    expect(ship.timesHit).toBe(2);
+  });
+
+  it("should return false for isSunk if timesHit is less than length", () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+  });
+
+  it("should return true for isSunk if timesHit is equal to or greater than length", () => {
+    const ship = new Ship(2);
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
 });
