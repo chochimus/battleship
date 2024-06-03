b7cf0ffa2ce8f9683e70842dd5cb28025fa794b6
chochimus <127354521+chochimus@users.noreply.github.com>
2024-06-03 18:25:23 -0400

    Create gameboard class functionality to place ships and receive attacks

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,1 +1,51 @@
-export class Gameboard {}
+export class Gameboard {
+  constructor() {
+    this.size = 10;
+    this.board = Array(this.size)
+      .fill(null)
+      .map(() => Array(this.size).fill(null));
+  }
+  place(ship, x, y, horizontal = true) {
+    if (
+      (horizontal && x + ship.length > this.size) ||
+      (!horizontal && y + ship.length > this.size)
+    ) {
+      throw new RangeError();
+    }
+
+    for (let i = 0; i < ship.length; i++) {
+      if (horizontal) {
+        if (this.board[y][x + i] !== null) {
+          throw new Error("Coordinates already occupied");
+        }
+      } else {
+        if (this.board[y + i][x] !== null) {
+          throw new Error("Coordinates already occupied");
+        }
+      }
+    }
+
+    for (let i = 0; i < ship.length; i++) {
+      if (horizontal) {
+        this.board[y][x + i] = ship;
+      } else {
+        this.board[y + i][x] = ship;
+      }
+    }
+
+    return ship;
+  }
+  receiveAttack(x, y) {
+    const target = this.board[y][x];
+    if (target === "miss" || target === "hit") {
+      throw new Error("coordinate has already been attacked");
+    } else if (target === null) {
+      this.board[y][x] = "miss";
+      return "miss!";
+    } else {
+      target.hit();
+      this.board[y][x] = "hit";
+      return target.isSunk() ? "sunk!" : "hit!";
+    }
+  }
+}
--- src/test.js
+++ src/test.js
@@ -1,4 +1,5 @@
 import { Ship } from "./ship";
+import { Gameboard } from "./gameboard";
 
 describe("Ship class", () => {
   it("should create a ship with a length property", () => {
@@ -31,5 +32,55 @@ describe("Ship class", () => {
     ship.hit();
     ship.hit();
     expect(ship.isSunk()).toBe(true);
+  });
+});
+
+describe("Gameboard", () => {
+  it("should return a gameboard object", () => {
+    expect(new Gameboard()).toBeInstanceOf(Gameboard);
+  });
+
+  it("should accept ship placement", () => {
+    const board = new Gameboard();
+    const cruiser = new Ship(3);
+    board.place(cruiser, 0, 0);
+    expect(board.board[0][0]).toBe(cruiser);
+    expect(board.board[0][1]).toBe(cruiser);
+    expect(board.board[0][2]).toBe(cruiser);
+  });
+
+  it("shouldn't allow placement into occupied spaces", () => {
+    const board = new Gameboard();
+    const cruiser = new Ship(3);
+    board.place(cruiser, 0, 0);
+    const ship = new Ship(2);
+    expect(() => board.place(ship, 0, 0)).toThrow(
+      new Error("Coordinates already occupied"),
+    );
+  });
+
+  describe("Receive attack function", () => {
+    const board = new Gameboard();
+    const cruiser = new Ship(3);
+    board.place(cruiser, 0, 0);
+    it("should accept attacks on ships", () => {
+      expect(board.receiveAttack(0, 0)).toBe("hit!");
+    });
+
+    it("should sink a ship if the hits = length", () => {
+      expect(board.receiveAttack(1, 0)).toBe("hit!");
+      expect(board.receiveAttack(2, 0)).toBe("sunk!");
+    });
+
+    it("shouldn't allow a sunken ship to be hit", () => {
+      expect(() => board.receiveAttack(0, 0)).toThrow(
+        new Error("coordinate has already been attacked"),
+      );
+    });
+
+    it("should be able to receive misses, and record their coord", () => {
+      expect(board.receiveAttack(4, 4)).toBe("miss!");
+      expect(board.board[4][4]).toBe("miss");
+    });
   });
 });
